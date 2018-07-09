b16f33304b8188e1cf1b07c71cb109855f7991f4
meowwwls <meowwwls@users.noreply.github.com>
2018-07-09 12:18:06 -0500

    Update .env vars

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ const { getLyrics } = require('./lyricadder/lyrics');
 const { randomNumber } = require('./helpers');
 
 const config = {
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 
 const Twitter = new twit(config);
